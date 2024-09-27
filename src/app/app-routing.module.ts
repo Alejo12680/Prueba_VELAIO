ea6dd55760731382b5e0d4f83a413bcb1ab47702
Alejo12680 <alejo152725@gmail.com>
2024-09-26 19:15:43 -0500

    interfaz_list-tareas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListTareaComponent } from './components/list-tarea/list-tarea.component';
+import { CreateTareaComponent } from './components/create-tarea/create-tarea.component';
 
 const routes: Routes = [
   {
     path:'',
     redirectTo:'listTareas',
     pathMatch:'full'
+  },
+  {
+    path:'listTareas',
+    component: ListTareaComponent
+  },
+  {
+    path:'createTarea',
+    component: CreateTareaComponent
   },
 
   // URL que no corresponde a ningun componente, es redirigido.
--- src/app/components/list-tarea/list-tarea.component.ts
+++ src/app/components/list-tarea/list-tarea.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class ListTareaComponent {
 
+  public tarea: any [] = [];
+
 }
